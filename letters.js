393707b2e0ec02bd5282300ad6cb0ff3d897373c
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-02-07 12:46:56 -0600

    packages added, letter const

---
+++ letters.js
@@ -0,0 +1,16 @@
+var inquirer = require('inquirer');
+
+
+//obj const to character guessed or placeholder
+function Letter(character, guess) {
+    this.character = character;
+    this.guess = guess;
+
+}
+
+//function that returns if the char. has been guessed or keeps the placeholder
+
+//function to take a character as an argument and checks it against underlying char .Updating the boolean value to true if it was guessed correctly
+
+
+
