45be98e7806ffec88c5b669c82f8563d20801ff1
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-02-09 18:22:04 -0600

    logic planned

---
+++ index.js
@@ -0,0 +1,3 @@
+//randomly selects a word and uses Word const to store it
+
+//prompts user for each guess + keeps track of remaining guesses
--- words.js
+++ words.js
@@ -1,3 +1,12 @@
 var words = require("./letters.js");
 
+// Word constructor
+function Word() {
+
+}
+
 //new 'letters' objects
+
+var toString = function() {
+    //returns string of the Word
+}
