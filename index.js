5017c483df2955cdd37485db66536b419f17e866
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-07-10 21:50:05 -0500

    constructor update

--- index.js
+++ index.js
@@ -3,29 +3,47 @@ var chalk = require("chalk");
 var words = require("./words.js");
 var list = require("./list.js");
 
+var randMovie;
+//Game constructor keeps track of score and controlls game flow
 
-//randomly selects a word and uses Word const to store it
-var randMovie = list[Math.floor(Math.random() * list.length)];
-console.log(randMovie);
-var guesses = [];
-var guessesLeft = randMovie.length + 5;
+function Game() {
 
-function ask() {
-//prompts user for each guess + keeps track of remaining guesses
-inquirer.prompt([
-    {
-        type: "input",
-        message: "Hello. Guess a letter. Come on, go ahead and guess a letter.",
-        name: "Letter"
-      }
+    this.play = function() {
+        this.guessesLeft = randMovie.length + 5;
+        this.nextWord();
+    };
+};
+
+//creates new Word object, ask for user guess
+this.nextWord = function() {
+
+    randMovie = list[Math.floor(Math.random() * list.length)];
+    this.currentMovie = new Word(randMovie);
+    console.log("\n" + this.currentMovie + "\n");
+    this.makeGuess();
+};
+
+// console.log(randMovie);
+// var guesses = [];
+// var guessesLeft = randMovie.length + 5;
+
+// function ask() {
+// //prompts user for each guess + keeps track of remaining guesses
+// inquirer.prompt([
+//     {
+//         type: "input",
+//         message: "Hello. Guess a letter. Come on, go ahead and guess a letter.",
+//         name: "Letter"
+//       }
 
 
 
-]).then(answers => {
-    Word(letters);
+// ]).then(answers => {
+//     Word(letters);
     
-    });
+//     });
 
-}
-ask();
-//randWord = new Letter();
+// }
+// ask();
+
+module.exports = Game;
